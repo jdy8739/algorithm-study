ba1bc13f24cbc8efc715d5a5204684f8e342a02c
psmjazz <48285417+psmjazz@users.noreply.github.com>
2025-03-04 21:03:56 +0900

    leetcode.100

---
+++ 100/psmjazz.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == nullptr && q == nullptr)
+            return true;
+        else if(p != nullptr && q == nullptr)
+            return false;
+        else if(p == nullptr && q != nullptr)
+            return false;
+        else if(p->val != q->val)
+            return false;
+        // else : p != nullptr && q != nullptr && p->val == q->val
+        
+        bool isLeftSame = isSameTree(p->left, q->left);
+        bool isRightSame = isSameTree(p->right, q->right);
+        return isLeftSame & isRightSame;
+    }
+};
